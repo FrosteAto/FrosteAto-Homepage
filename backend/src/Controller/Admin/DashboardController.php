6ab67f74d7921d663bc07cac387f9f6d3d2c3c5a
FrosteAto <FrosteAto.exparrot@protonmail.com>
2026-08-30 14:29:35 +0100

    fix: use an icon that actually exists in the bundled Font Awesome set
    Review of Task 4 found fa-map-marker-slash isn't in the bundled CSS
    (backend/public/bundles/easyadmin/app.*.css has no match), so the menu
    item rendered with no icon. Swapped to fa-location-dot, confirmed present.

--- backend/src/Controller/Admin/DashboardController.php
+++ backend/src/Controller/Admin/DashboardController.php
@@ -47,7 +47,7 @@ class DashboardController extends AbstractDashboardController
         yield MenuItem::linkTo(PhotoCrudController::class, 'Photos', 'fa fa-image');
         yield MenuItem::linkToRoute('Bulk Upload', 'fa fa-upload', 'admin_photos_bulk_upload');
         yield MenuItem::linkToRoute('Generate Thumbnails', 'fa fa-images', 'admin_photos_backfill_thumbnails');
-        yield MenuItem::linkToRoute('Strip GPS Data', 'fa fa-map-marker-slash', 'admin_photos_backfill_gps');
+        yield MenuItem::linkToRoute('Strip GPS Data', 'fa fa-location-dot', 'admin_photos_backfill_gps');
         yield MenuItem::linkToRoute('Backfill Camera Settings', 'fa fa-sliders', 'admin_photos_backfill_settings');
         yield MenuItem::linkTo(AlbumCrudController::class, 'Albums', 'fa fa-folder');
         yield MenuItem::linkTo(TagCrudController::class, 'Tags', 'fa fa-tag');
